79802582e5e522b0715dd1435c110b5d30db53eb
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-09-25 13:07:31 -0300

    colocando uma mensagem caso seja passado letras ao inves de numeros

--- javaScript.js
+++ javaScript.js
@@ -1,20 +1,24 @@
 function calcular(tipo, valor) {
-  if (tipo === 'acao') {
+  try {
+    if (tipo === 'acao') {
 
-    if (valor === 'C') {
-      document.getElementById('resultado').value = '';
-    }
-
-    if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
+      if (valor === 'C') {
+        document.getElementById('resultado').value = '';
+      }
+  
+      if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
+        document.getElementById('resultado').value += valor;
+      }
+  
+      if (valor === '=') {
+        let valor_campo = eval(document.getElementById('resultado').value);
+        document.getElementById('resultado').value = valor_campo;
+      }
+  
+    } else if (tipo === 'valor') {
       document.getElementById('resultado').value += valor;
     }
-
-    if (valor === '=') {
-      let valor_campo = eval(document.getElementById('resultado').value);
-      document.getElementById('resultado').value = valor_campo;
-    }
-
-  } else if (tipo === 'valor') {
-    document.getElementById('resultado').value += valor;
+  } catch(e) {
+    document.getElementById('resultado').value = "Valores informados inválidos";
   }
 };
